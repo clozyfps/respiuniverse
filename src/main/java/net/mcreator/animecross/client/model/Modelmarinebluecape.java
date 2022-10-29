402f114445cccfe98b1a2c34177b0795a64f2e58
Sikues <113269179+Sikues@users.noreply.github.com>
2022-10-29 01:24:46 -0400

    MCreator Merge commit 'ea0cad1551b9ac998a0f1795f20df239619cfd6c' into a55d8970a981283c67e64d3e63022e04953c024f

--- src/main/java/net/mcreator/animecross/client/model/Modelmarinebluecape.java
+++ src/main/java/net/mcreator/animecross/client/model/Modelmarinebluecape.java
@@ -9,6 +9,7 @@ import net.minecraft.client.model.geom.builders.CubeListBuilder;
 import net.minecraft.client.model.geom.builders.CubeDeformation;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.EntityModel;
 
 import com.mojang.blaze3d.vertex.VertexConsumer;
